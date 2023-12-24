bdd1f2b919b53f96ed43ade1a715e92464554b34
Paul <jpdcands211@gmail.com>
2023-12-24 15:53:37 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/practice_1/DetailsScreen.kt
@@ -0,0 +1,11 @@
+package com.example.practice_1
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun DetailsScreen() {
+    Text("This is the Details Screen")
+}
